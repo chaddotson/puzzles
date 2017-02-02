eccb2379c4d6df21b597dc1e70ce62d8a46107b5
Eduardo <eddie@void_t.void>
2017-02-01 20:17:34 -0500

    C++ implementations: recursive, non-recursive

--- run.sh
+++ run.sh
@@ -3,40 +3,42 @@
 for i in {8..12}; do
     echo "=== testing $i ==="
 
-    if [ -x $(which node) ]
+    which node &>/dev/null && if [ -x $(which node) ]
         then
         echo "node: $(node javascript_n_queens_solver.js $i)"
     fi
 
-    if [ -x $(which luajit) ]
+    which luajit &>/dev/null && if [ -x $(which luajit) ]
         then
         echo "lua : $(board_size=$i luajit luajit_n_queens_solver.lua)"
     fi
 
-    if [ -x $(which pypy) ]
+    which pypy &>/dev/null && if [ -x $(which pypy) ]
         then
         echo "pypy: $(pypy python_n_queens_solver.py $i)"
     fi
 
-    if [ -x $(which python) ]
+    which python &>/dev/null && if [ -x $(which python) ]
         then
         echo "python: $(python python_n_queens_solver.py $i)"
     fi
 
-    if [ -x $(which scala) ]
+    which scala &>/dev/null && if [ -x $(which scala) ]
         then
         echo "scala: $(scala scala_n_queens_solver.scala $i)"
     fi
 
-    if [ -x $(which ruby) ]
+    which ruby &>/dev/null && if [ -x $(which ruby) ]
         then
         echo "ruby: $(ruby ruby_n_queens_solver.rb $i)"
     fi
 
-    if [[ -x $(which dart) ]]
+    which dart &> /dev/null && if [ -x $(which dart) ]
     then
       echo "dart: $(dart dart_n_queens_solver.dart $i)"
     fi
+
+    echo "C: $(./c_solver $i)"
 
     echo
 
